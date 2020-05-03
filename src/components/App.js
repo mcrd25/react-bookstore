0bf9c0ebb21476abecd050a0555d5bda4edd22d7
mcrd25 <mayacrd25@gmail.com>
2020-05-02 23:01:30 -0400

    Imported App component into index.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,6 @@ import BooksForm from '../containers/BooksForm';
 import BooksList from '../containers/BooksList';
 
 const App = () => {
-
   return (
     <div>
       <BooksList />
--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/App';
+import './index.css';
 
-ReactDOM.render(<h1>React bookstore</h1>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
