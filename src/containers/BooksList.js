f32bff61dcdaeb200704740c3656dce8684b8b3d
mcrd25 <mayacrd25@gmail.com>
2020-05-02 23:39:11 -0400

    Converted Booklist to stateful compoenent without reduxer

--- src/containers/BooksList.js
+++ src/containers/BooksList.js
@@ -1,11 +1,26 @@
 import React from 'react';
 
+class BooksList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      books: [{ name: 'Title' }, { name: 'Title' }],
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
 
-const BooksList = () => {
-
-  return (
-    <div>BookList</div>
-  );
-};
+  render() {
+    const { books } = this.state;
+    return (
+      <div>
+        {
+          books.map(book => (
+            <div key={book}>{book.name}</div>
+          ))
+        }
+      </div>
+    );
+  }
+}
 
 export default BooksList;
