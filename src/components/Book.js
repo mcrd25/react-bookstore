b84728eafd416680a3633d814f4048fd0e59b202
mcrd25 <mayacrd25@gmail.com>
2020-07-10 14:10:04 -0400

    Fixed td tag issue with Book component

--- src/components/Book.js
+++ src/components/Book.js
@@ -5,10 +5,12 @@ import './Book.css';
 const Book = ({ book, handleBookRemove }) => {
   const { id, title, category } = book;
   return (
-    <tr id={id} className="book card mb-3 px-4 py-4 bg-white d-flex align-items-start flex-column">
-      <h5 className="text-secondary book-category">{category}</h5>
-      <h1 className="mt-0 pt-0 book-title">{title}</h1>
-      <button className="btn btn-link px-0 mb-0 pb-0 mt-3" type="button" onClick={() => handleBookRemove(book)}>Remove</button>
+    <tr>
+      <td id={id} className="book card mb-3 px-4 py-4 bg-white d-flex align-items-start flex-column">
+        <h5 className="text-secondary book-category">{category}</h5>
+        <h1 className="mt-0 pt-0 book-title">{title}</h1>
+        <button className="btn btn-link px-0 mb-0 pb-0 mt-3" type="button" onClick={() => handleBookRemove(book)}>Remove</button>
+      </td>
     </tr>
   );
 };
